0d414afbdd01fca196bb48ae471b0b8d23e39044
Larry Bluestoneapps <larry@bluestoneapps.com>
2023-12-11 17:22:44 -0500

    added variable content

--- includes/openai-gpt-functions.php
+++ includes/openai-gpt-functions.php
@@ -5,12 +5,22 @@ function openai_gpt_generate_text($prompt, $knowledge_data)
   $api_key = trim(get_option('openai_gpt_api_key'));
   $stored_instructions = get_option('openai_gpt_high_level_instructions', '');
   $custom_urls = get_option('custom_urls', ''); // Ensure this key matches the one used in update_option
-  $custom_urls = 'Use only the information from ' . stripslashes($custom_urls) . ' to formulate your responses.';
+  $custom_urls = 'Use the information from ' . stripslashes($custom_urls) . ' to formulate your responses.';
   $stored_instructions = stripslashes($stored_instructions);
 
   $knowledge_data = get_option('openai_gpt_knowledge_data', '');
 
-  $system_message_content = $stored_instructions . "\n" . $custom_urls . "\n\n" . $knowledge_data;
+  $variables = '- Make sure to use only the provided data to produce a coherent answer
+  - If the provide data provides insufficient information, refuse to answer the question and redirect the user to https://bluestoneapps.com for more help. 
+  - If the provide data Title or Snippet is unrelated or irrelevant to the question, do not respond and just say you do not know.
+  - Cite the provide data using [${number}] notation in your answer.
+  - If the response contains list of items such as services, then always list the items in a numbered list in your answer.
+
+  Given the conversation, only rephrase the last message to be a standalone statement in 2nd persons perspective. Make sure you include only the relevant parts of the conversation required to answer the follow-up question, and not the answer to the question. If the conversation is irrelevant to the current question being asked, discard it.
+  
+  Extract only rare terms like part numbers from the message, which can be helpful in document searching. If there are no uncommon keywords, return an empty string. Do not use quotation marks . Account for any spelling errors in the message to generate relevant keywords. Output the keywords as space separated instead of using commas. Limit the keywords to 5 words.';
+
+  $system_message_content = $stored_instructions . "\n" . $variables  . "\n" . $custom_urls . "\n\n" . $knowledge_data;
 
   // Debug: Check API Key and Instructions
   error_log('API Key: ' . $api_key);
